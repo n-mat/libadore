0df2d28d7c1d3d7a9e590646cb68754025a6a6e8
Matthias <ngit@mailbox.org>
2023-09-21 22:27:19 +0200

    Update planning_request.h

--- libadore/adore/fun/include/adore/fun/tac/planning_request.h
+++ libadore/adore/fun/include/adore/fun/tac/planning_request.h
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (C) 2017-2020 German Aerospace Center (DLR). 
+ * Copyright (C) 2017-2023 German Aerospace Center (DLR). 
  * Eclipse ADORe, Automated Driving Open Research https://eclipse.org/adore
  *
  * This program and the accompanying materials are made available under the 
@@ -10,6 +10,7 @@
  *
  * Contributors: 
  *   Daniel Heß - a request to plan a trajectory
+ *   Matthias Nichting
  *********************************************************************************/
 #pragma once
 #include <adore/fun/setpointrequest.h>
@@ -24,9 +25,11 @@ struct PlanningRequest
     double t_planning_start;        /**<plannning start time*/
     double t_planning_end;          /**<terminate planning for given iteration at this time*/
     double t_emergency_start;       /**<*/
+    double v_ref_percentage_of_v_max; /**<request a plan with specific percentage of v_max as reference velocity*/
+    bool allow_for_cooperation;  
     SetPoint initial_state;          /**<the initial state of the plan*/
     PlanningRequest()
-    :iteration(0),t_planning_start(0.0),t_planning_end(0.0),t_emergency_start(0.0)
+    :iteration(0),t_planning_start(0.0),t_planning_end(0.0),t_emergency_start(0.0),v_ref_percentage_of_v_max(0.0),allow_for_cooperation(false)
     {}
 };  
 }
